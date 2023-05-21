4b4c7408f8aeb8a55bbf589e6c20940b9eee3069
pjione <a41924250@gmail.com>
2023-05-22 07:13:15 +0900

    1330 두 수 비교하기

--- src/class1/Main.java
+++ src/class1/Main.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double num1 = in.nextInt();
+        //1008
+      /*  double num1 = in.nextInt();
         double num2 = in.nextInt();
-        System.out.println(num1/num2);
+        System.out.println(num1/num2);*/
+
+        //1330
+      /*  long a = in.nextInt();
+        long b = in.nextInt();
+
+        String result = a > b ? ">" : a < b ? "<" : "==";
+
+        System.out.println(result);*/
     }
 }
