3ed8b9527b2ec906d24183e11f155525a1461fbe
pjione <a41924250@gmail.com>
2023-05-22 06:11:05 +0900

    a/b

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double num1 = in.nextInt();
+        double num2 = in.nextInt();
+        System.out.println(num1/num2);
+    }
+}
