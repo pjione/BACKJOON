5b7e2a6651900ee065cb638f8d56538bc0a16759
pjione <a41924250@gmail.com>
2023-05-23 03:53:43 +0900

    최소, 최대 - 10818

--- src/class1/Main.java
+++ src/class1/Main.java
@@ -1,6 +1,9 @@
 package class1;
 import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -18,14 +21,24 @@ public class Main {
         System.out.println(result);*/
 
         //11720
-        int n = in.nextInt();
+        /*int n = in.nextInt();
         in.nextLine();
         String n2 = in.nextLine();
         String[] split = n2.split("");
 
         int sum = Arrays.stream(split).mapToInt(Integer::parseInt).sum();
 
-        System.out.println(sum);
+        System.out.println(sum);*/
+
+        //10818
+        int cnt = in.nextInt();
+        in.nextLine();
+        String nums = in.nextLine();
+        String[] arrayNums = nums.split(" ");
+        int a = Arrays.stream(arrayNums).mapToInt(Integer::parseInt).min().orElse(-1);
+        int b = Arrays.stream(arrayNums).mapToInt(Integer::parseInt).max().orElse(-1);
+        System.out.println(a + " " + b);
+
 
     }
 
